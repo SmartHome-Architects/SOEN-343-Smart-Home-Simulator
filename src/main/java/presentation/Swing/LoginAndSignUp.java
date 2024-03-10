33b6bb9cfe55e4564cecdf9387d4d6b8831d7766
Rana Gohar <94134735+RanaGohar@users.noreply.github.com>
2024-03-09 21:41:33 -0500

    Update LoginAndSignUp.java

--- src/main/java/presentation/Swing/LoginAndSignUp.java
+++ src/main/java/presentation/Swing/LoginAndSignUp.java
@@ -15,19 +15,20 @@ public class LoginAndSignUp {
         login.setSignUpActionListener(e -> {
             login.setVisible(false);
             signup.setVisible(true);
+            signup.setLocationRelativeTo(null); // Set location after making it visible
         });
 
         // Action listener for "Back to Login" button in the signup page
         signup.setBackToLoginActionListener(e -> {
             signup.setVisible(false);
             login.setVisible(true);
+            login.setLocationRelativeTo(null); // Set location after making it visible
         });
 
-        login.setVisible(true);
         login.pack();
         login.setLocationRelativeTo(null);
+        login.setVisible(true);
 
         signup.pack();
-        signup.setLocationRelativeTo(null);
     }
 }
