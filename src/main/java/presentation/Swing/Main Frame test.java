23aeb86df4293b3c1a2ee26e01b7ab20046015c6
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-26 14:58:27 -0400

    Update Main Frame test.java

--- src/main/java/presentation/Swing/Main Frame test.java
+++ src/main/java/presentation/Swing/Main Frame test.java
@@ -7,3 +7,37 @@ import domain.dateTime.Time;
 import domain.editbutton.EditHouseInhabitantsDialog;
 import domain.house.House;
 
+import domain.smartHomeSimulator.modules.SmartHomeHeating;
+import domain.user.LoggedInUser;
+
+import presentation.Swing.SHC.SHCDisplay;
+import presentation.Swing.command.AddProfileCommand;
+import presentation.Swing.command.DeleteProfileCommand;
+import presentation.Swing.command.EditProfileCommand;
+import presentation.Swing.command.ProfileManager;
+import presentation.Swing.command.UserAccountManager;
+import presentation.Swing.managePermission.PermissionsPopup;
+
+import javax.swing.*;
+
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.List;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MainFrame {
+
+    private JPanel WindowContainer;
+    private JPanel titleContainer;
+    private JLabel title;
+    private JPanel ProfileBoard;
+    private JPanel Modules;
+    private JPanel houseImage;
+    private JPanel consoleOutput;
+    private JTabbedPane tabbedPane1;
+    
