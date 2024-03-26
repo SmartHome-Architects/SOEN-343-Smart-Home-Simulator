cb28640cbc052e837ab62d373d755a65283baee1
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-26 14:57:54 -0400

    Create Main Frame test.java

---
+++ src/main/java/presentation/Swing/Main Frame test.java
@@ -0,0 +1,9 @@
+package presentation.Swing;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.dateTime.Date;
+import domain.dateTime.Time;
+import domain.editbutton.EditHouseInhabitantsDialog;
+import domain.house.House;
+
