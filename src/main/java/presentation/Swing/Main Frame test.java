bb0b26e2427bae83c6be796e14d0776e4c26494d
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-26 14:59:11 -0400

    Update Main Frame test.java

--- src/main/java/presentation/Swing/Main Frame test.java
+++ src/main/java/presentation/Swing/Main Frame test.java
@@ -51,5 +51,66 @@ public class MainFrame {
     private JButton buttonOff;
     private JLabel userTag;
     private JLabel locationTag;
+    private JLabel temperature;
+    private JLabel date;
+    private JLabel time;
+    private JButton buttonLogOut;
+    private JPanel SetTimeDate;
+    private JTextField TimeText;
+    private JTextField DateText;
+    private JTabbedPane ManageUsers;
+    private JPanel Add;
+    private JPanel Delete;
+    private JPanel Edit;
+    private JTextField NewUsername;
+    private JTextField NewEmail;
+    private JRadioButton NewparentRadioButton;
+    private JRadioButton NewchildRadioButton;
+    private JRadioButton NewguestRadioButton;
+    private JTextField DeleteUser;
+    private JLabel DeleteUsername;
+    private JPasswordField NewPassword;
+    private JTextField OldUsername;
+    private JTextField UpdateUsername;
+    private JTextField UpdateEmail;
+    private JPasswordField UpdatePassword;
+    private JRadioButton UpdateparentRadioButton;
+    private JRadioButton UpdatechildRadioButton;
+    private JRadioButton UpdateguestRadioButton;
+    private JButton Add_Profile;
+    private JButton Delete_Profile;
+    private JButton Edit_Profile;
+    private JLabel houseLayoutLabel;
+
+    private ImageIcon houseLayout;
+    private JPanel kitchenLightPanel;
+    private JPanel livingRoomLightPanel;
+    private JPanel bedroom2LightPanel;
+    private JPanel bathroomLightPanel;
+    private JPanel garageLightPanel;
+    private JPanel bedroom1LightPanel;
+    private JPanel hallwayLightPanel;
+    private JPanel backLightPanel;
+    private JPanel frontLightPanel;
+    private JLabel livingLightLabel;
+    private JLabel bedroom2LightLabel;
+    private JLabel bedroom1LightLabel;
+    private JLabel hallwayLightLabel;
+    private JLabel backLightLabel;
+    private JLabel bathroomLightLabel;
+    private JLabel garageLightLabel;
+    private JLabel frontLightLabel;
+    private JLabel kitchenLightLabel;
+
+    private JButton editButton;
+
+    private JTable table1;
+    private JTable table2;
+    private JTable table3;
+    private JTable table4;
+    private JTable table5;
+    private JPanel checkBoxPanel;
+    private JComboBox comboBox;
+    
     
 
