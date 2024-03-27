7359bf63d738a1ac2f4c5e8a96c33d17216e97b3
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 11:54:02 -0400

    Creating a Junit test for HeaterTest class

--- src/test/java/domainTest/HeaterTest.java
+++ src/test/java/domainTest/HeaterTest.java
@@ -1,4 +1,21 @@
 package domainTest;
+import domain.sensors.Heater;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class HeaterTest {
+
+
+    @Test
+    public void testConstructor() {
+
+        int expectedId = 1;
+
+
+        Heater heater = new Heater(expectedId);
+
+        // Assert
+        assertNotNull(heater);
+
+    }
 }
