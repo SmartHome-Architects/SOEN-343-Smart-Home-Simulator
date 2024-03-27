08418fe8d10affbb88b526c62c011c2cbc8be8cc
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 12:30:23 -0400

    Creating a Junit test for SmartHomeCoreHeating Test class

--- src/test/java/domainTest/SmartHomeHeatingTest.java
+++ src/test/java/domainTest/SmartHomeHeatingTest.java
@@ -1,4 +1,23 @@
 package domainTest;
 
+import domain.smartHomeSimulator.modules.SmartHomeHeating;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SmartHomeHeatingTest {
+
+    @Test
+    public void testSmartHomeHeatingInitialization() {
+        // Create an instance of SmartHomeHeating
+        SmartHomeHeating smartHomeHeating = new SmartHomeHeating();
+
+        // Verify that the SmartHomeHeating instance is not null
+        assertNotNull(smartHomeHeating);
+
+        // Verify that the isActive flag is initially set to false
+        assertFalse(smartHomeHeating.isActive());
+    }
+
+
 }
