cce8040bfbaf5fe923e488fc6a25c90fff99373d
Rana Gohar <94134735+RanaGohar@users.noreply.github.com>
2024-03-27 10:38:14 -0400

    junit test for PermissionsPopup class

---
+++ src/test/java/domainTest/PermissionsPopupTest.java
@@ -0,0 +1,43 @@
+package domainTest;
+
+import org.junit.Test;
+import presentation.Swing.managePermission.PermissionsPopup;
+
+import javax.swing.table.DefaultTableModel;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import static org.junit.Assert.assertEquals;
+
+public class PermissionsPopupTest {
+
+    @Test
+    public void testUpdateTableModel() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        DefaultTableModel model = new DefaultTableModel(new Object[]{"User Type", "Outside Permission", "Inside Permission"}, 0);
+        PermissionsPopup permissionsPopup = new PermissionsPopup();
+
+        // Get the updateTableModel method using reflection
+        Method updateTableModelMethod = PermissionsPopup.class.getDeclaredMethod("updateTableModel", String.class, DefaultTableModel.class);
+        updateTableModelMethod.setAccessible(true); // Set accessible since the method is private
+
+        // Invoke the method with the desired parameters
+        updateTableModelMethod.invoke(permissionsPopup, "Door", model);
+
+        // Assert the number of rows added to the table model
+        assertEquals(3, model.getRowCount());
+
+        // Assert the values in the first row
+        assertEquals("Parent", model.getValueAt(0, 0));
+        assertEquals(true, model.getValueAt(0, 1));
+        assertEquals(true, model.getValueAt(0, 2));
+
+        // Assert the values in the second row
+        assertEquals("Children", model.getValueAt(1, 0));
+        assertEquals(false, model.getValueAt(1, 1));
+        assertEquals(false, model.getValueAt(1, 2));
+
+        // Assert the values in the third row
+        assertEquals("Guest", model.getValueAt(2, 0));
+        assertEquals(false, model.getValueAt(2, 1));
+        assertEquals(false, model.getValueAt(2, 2));
+    }
+}
