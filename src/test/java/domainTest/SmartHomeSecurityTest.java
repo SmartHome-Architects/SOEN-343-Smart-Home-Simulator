7288aad850eae775af225b3fa75de88548e7c09d
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 13:18:43 -0400

    Creating a Junit test for SmartHomeSecurity Test class

--- src/test/java/domainTest/SmartHomeSecurityTest.java
+++ src/test/java/domainTest/SmartHomeSecurityTest.java
@@ -1,4 +1,20 @@
 package domainTest;
 
+import domain.smartHomeSimulator.modules.SmartHomeSecurity;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class SmartHomeSecurityTest {
+
+    @Test
+    public void testSmartHomeSecurityInitialization() {
+        // Create an instance of SmartHomeSecurity
+        SmartHomeSecurity smartHomeSecurity = new SmartHomeSecurity();
+
+        // Verify that the SmartHomeSecurity instance is not null
+        assertNotNull(smartHomeSecurity);
+    }
+
+    // You can add more test methods to test other functionalities of SmartHomeSecurity class
 }
