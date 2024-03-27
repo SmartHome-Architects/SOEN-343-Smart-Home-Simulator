d3cbe639d7a4b2c2d4180f94e3fd2a3175f4b153
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 13:46:58 -0400

    user permission

--- src/test/java/domainTest/UserPermissionInitializerTest.java
+++ src/test/java/domainTest/UserPermissionInitializerTest.java
@@ -1,4 +1,25 @@
+
 package domainTest;
 
+import domain.Permission.Permission;
+import domain.user.UserPermissionInitializer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class UserPermissionInitializerTest {
+
+    @Test
+    public void testInitializePermission() {
+        String doorPermissionFile = "door_permissions.txt";
+        String lightPermissionFile = "light_permissions.txt";
+        String windowPermissionFile = "window_permissions.txt";
+        String shhPermissionFile = "shh_permissions.txt";
+        String userType = "admin"; // or any user type you want to test
+
+        Permission permission = UserPermissionInitializer.initializePermission(doorPermissionFile, lightPermissionFile, windowPermissionFile, shhPermissionFile, userType);
+
+        assertNotNull(permission);
+        // Add more assertions as needed to test the initialized Permission object
+    }
 }
