7ea7350ff7c569834afd97c9c73129f2f8b8dc55
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 12:26:45 -0400

    Creating a Junit test for SmartHomeCoreFunctionality Test class

--- src/test/java/domainTest/SmartHomeCoreFunctionalityTest.java
+++ src/test/java/domainTest/SmartHomeCoreFunctionalityTest.java
@@ -1,4 +1,22 @@
 package domainTest;
 
+import org.junit.Test;
+import domain.smartHomeSimulator.modules.SmartHomeCoreFunctionality;
+
+import static org.junit.Assert.*;
+
 public class SmartHomeCoreFunctionalityTest {
+
+    @Test
+    public void testSmartHomeCoreFunctionality() {
+        // Create an instance of SmartHomeCoreFunctionality
+        SmartHomeCoreFunctionality smartHomeCore = new SmartHomeCoreFunctionality();
+
+        // You can add test cases here to verify the functionality of SmartHomeCoreFunctionality class
+        // For example:
+        // assertEquals(expectedValue, actualValue);
+        // assertTrue(condition);
+        // assertFalse(condition);
+        // etc.
+    }
 }
