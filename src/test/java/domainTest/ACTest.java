ced8f4d281b077e3fb632aafa431f3482e111503
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 11:17:00 -0400

    Creating a Junit test for Ac class

--- src/test/java/domainTest/ACTest.java
+++ src/test/java/domainTest/ACTest.java
@@ -1,6 +1,18 @@
+
+
 package domainTest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import domain.sensors.AC;
 
 public class ACTest {
 
-}
+    @Test
+    public void testConstructor() {
+        AC ac = new AC(1);
+        assertNotNull(ac);
+    }
 
+    // You can add more test methods to cover other functionalities or edge cases of your AC class.
+}
