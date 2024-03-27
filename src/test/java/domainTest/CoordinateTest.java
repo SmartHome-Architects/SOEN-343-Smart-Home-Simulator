b6f2733b632f9dfcf8ad91f7ef47ac59071f4063
Rana Gohar <94134735+RanaGohar@users.noreply.github.com>
2024-03-27 10:46:08 -0400

    junit test coordinate class

--- src/test/java/domainTest/CoordinateTest.java
+++ src/test/java/domainTest/CoordinateTest.java
@@ -1,4 +1,23 @@
 package domainTest;
 
+import domain.house.Coordinate;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CoordinateTest {
+
+    @Test
+    public void testGetX() {
+        Coordinate coordinate = new Coordinate(5, 10);
+        assertEquals(5, coordinate.getX());
+    }
+
+    @Test
+    public void testGetY() {
+        Coordinate coordinate = new Coordinate(5, 10);
+        assertEquals(10, coordinate.getY());
+    }
+
+    // Add more test cases as needed
 }
