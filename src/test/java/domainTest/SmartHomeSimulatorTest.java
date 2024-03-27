44a97097126c1dec41c72673d4abf657bdebe2d9
marinaGirgis <104663430+marinaGirgis@users.noreply.github.com>
2024-03-27 14:27:32 -0400

    smart home simulator

--- src/test/java/domainTest/SmartHomeSimulatorTest.java
+++ src/test/java/domainTest/SmartHomeSimulatorTest.java
@@ -1,4 +1,15 @@
 package domainTest;
 
+import domain.smartHomeSimulator.SmartHomeSimulator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SmartHomeSimulatorTest {
+
+    @Test
+    public void testSmartHomeSimulatorConstructor() {
+        SmartHomeSimulator smartHomeSimulator = new SmartHomeSimulator();
+        assertNotNull(smartHomeSimulator);
+    }
 }
