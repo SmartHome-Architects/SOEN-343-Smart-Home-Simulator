3872ca6e461e6b1bce4a56ad52b67731752ab51c
Rana Gohar <94134735+RanaGohar@users.noreply.github.com>
2024-03-28 09:35:52 -0400

    junit for main class

--- src/test/java/ApplicationTest/MainTest.java
+++ src/test/java/ApplicationTest/MainTest.java
@@ -1,4 +1,23 @@
 package ApplicationTest;
 
+import Application.Main;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class MainTest {
+
+    @Test
+    public void testMainMethod() {
+        // Execute the main method of the Main class
+        // Ensure that it does not throw any exceptions
+        try {
+            Main.main(new String[]{});
+            // If the main method is executed without exceptions,
+            // we assume that the GUI is launched successfully
+            assertTrue(true);
+        } catch (Exception e) {
+            // If an exception is caught, fail the test
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
 }
