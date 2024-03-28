7c8ecf765c5c1658147d3039ad33524bb75f6980
Rana Gohar <94134735+RanaGohar@users.noreply.github.com>
2024-03-27 23:28:53 -0400

    junit

--- src/test/java/presentationTest/LoginAndSignUpTest.java
+++ src/test/java/presentationTest/LoginAndSignUpTest.java
@@ -1,4 +1,19 @@
 package presentationTest;
 
+import org.junit.Test;
+import presentation.Swing.LoginAndSignUp.LoginAndSignUp;
+
+import static org.junit.Assert.*;
+
 public class LoginAndSignUpTest {
+
+    @Test
+    public void testMainMethod() {
+        // Test that the main method does not throw any exceptions
+        try {
+            LoginAndSignUp.main(new String[0]);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
 }
